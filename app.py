21975961a588dcec22167e9d6f65bcc4afa1cee9
Ignacio Rubio Majano <rubiomajano.i@gmail.com>
2023-09-08 17:26:22 +0200

    Working interactive front-end. TODO: 1) Connect to API; 2) Try combining Streamlit with Dash to update n_topics in real time; 3) Add interactivity, e.g. topic representation when hovering over topic names; 4) Add content to the website

--- app.py
+++ app.py
@@ -1,26 +1,57 @@
 import streamlit as st
 import requests
-
-import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 import pandas as pd
 
 st.markdown('# News Cycle Aggregator')
-## This is a sub header
 
-#url = 'https://blabla'
+#url = 'https://url-to-api.com'
 #df = requests.get(url).json()
 
-df = pd.DataFrame({
-    'first column': list(range(1, 11)),
-    'second column': np.arange(10, 101, 10)
-})
+# Temporarily reading data from file
+df = pd.read_json("data.json", orient='table')
 
-# this slider allows the user to select a number of lines
-# to display in the dataframe
-# the selected value is returned by st.slider
-line_count = st.slider('Select a line count', 1, 10, 3)
+# Initialize number of topics
+n_topics = 0
 
-# and used to select the displayed lines
-head_df = df.head(line_count)
 
-head_df
+
+def update_df(df, n_topics):
+    # Select first n topics
+    df = df.iloc[:n_topics, :]
+    # Convert document counts to percentages
+    df = df.apply(lambda x: x / x.sum(), axis=0).fillna(0)
+    return df
+
+def create_figure(x, y, labels):
+    fig = go.Figure()
+    for y_, label in zip(y, labels):
+        fig.add_trace(
+            go.Scatter(x=x,
+                       y=y_,
+                       name=label,
+                       mode='lines',
+                       line=dict(width=0.5),
+                       stackgroup='one',
+                       groupnorm='percent')
+            )
+    fig.update_layout(showlegend=True,
+                      xaxis_type='category',
+                      yaxis=dict(type='linear', range=[1, 100],
+                                 ticksuffix='%'))
+    return fig
+
+with st.form("submit_form"):
+    slider_val = st.slider('Select a number of topics', 1, len(df), 10)
+
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        n_topics = slider_val
+        df = update_df(df, n_topics)
+        # Get x, y and labels
+        x = df.columns.values.tolist()
+        y = df.values.tolist()
+        labels = df.index.get_level_values('Name').tolist()
+        fig = create_figure(x, y, labels)
+        st.plotly_chart(fig, use_container_width=True)
