76452f5e4d1c751dea245a1ac1baba606567664b
Rafal Klosek <rafklosek@me.com>
2023-09-13 17:32:49 +0200

    ss

--- app.py
+++ app.py
@@ -2,65 +2,53 @@ import streamlit as st
 import requests
 import plotly.graph_objects as go
 import pandas as pd
-
-MAX_TOPICS = 20
-REQUEST_URL = 'https://news-cycle-aggregator-fudwhg6x5q-ew.a.run.app/get-processed'
-
-st.set_page_config(layout="wide", page_title='NewsWatch')
-
-# # Custom CSS for styling
-# st.markdown("""
-# <style>
-# body {
-#     background-color: #F3ECF3;
-#     color: #121640;
-#     font-family: sans-serif
-# }
-# .sidebar .sidebar-content {
-#     background-color: #121640;
-#     color: #F3ECF3;
-# }
-# </style>
-# """, unsafe_allow_html=True)
+from datetime import date
+from random import shuffle
 
 def add_logo():
     st.markdown(
         """
         <style>
             [data-testid="stSidebarNav"] {
-                background-image: url(https://i.postimg.cc/G37XtY0J/3.png);
-                background-size: 300px;
+                background-image: url(https://i.postimg.cc/QNH0Rdz4/2.png);
+                background-size: 250px;
                 width: 900;
                 height: 900px;
                 background-repeat: no-repeat;
+                background-position: center;
                 background-position-x: 60%;
-                background-position-y: top;
-                padding-top: 120px;
+                background-position-y: 50px;
+                padding-top: 250px;
             }
         </style>
         """,
         unsafe_allow_html=True,
     )
+
+st.set_page_config(layout="wide", page_title='NewsWatch')
+
+MAX_TOPICS = 20
+REQUEST_URL = 'https://news-cycle-aggregator-fudwhg6x5q-ew.a.run.app/get-processed'
+
+submitted = False # Initialize Submit button state
+slider_val = 5 # Initialize number of topics
+
+with st.sidebar:
+    with st.form(key='my_form'):
+        slider_val = st.slider('Select number of top news topics to visualize', 1, MAX_TOPICS, 5)
+        submitted = st.form_submit_button(label='Submit')
+
+st.title("NewsWatch")
+st.subheader("Visualize last week's trending topics in world news")
+st.markdown(f"""
+**NewsWatch** is a news aggregator that collects news from last week from a wide range of RSS feeds and displays them in a single place.\n
+The app uses a topic modeling algorithm to extract topics from news articles and then visualizes the results in a streamgraph.
+""")
+
 add_logo()
-# change page names in the sidebar to "About" and "Sources"
 
-
-st.title("NewsWatch Aggregator")
-
-# display the title "NewsWatch Aggregator" using Righteous Regular font
-#st.markdown("<h1 style='text-align: center; color: #000000; font-size: 50px; font-family: Righteous, sans-serif;'>NewsWatch Aggregator</h1>", unsafe_allow_html=True)
-
-# Temporarily reading data from file
-#df = pd.read_json("data.json", orient='table')
-
-
-
-# Initialize number of topics
-slider_val = 5
-
-def process_request():
-    r = requests.get(REQUEST_URL).json()
-    df = pd.read_json(r)
+@st.cache
+def process_request(df):
     df['date'] = pd.to_datetime(df['date']).dt.date #add column with only date, without time
     new_df = pd.DataFrame()
     for topic in df.groupby('topic'):
@@ -73,25 +61,64 @@ def process_request():
     new_df.sort_index(inplace = True, axis=1)
     return new_df
 
-def update_df(df, n_topics):
-    # Select first n topics
-    df = df.iloc[:n_topics, :]
-    # Convert document counts to percentages
-    df = df.apply(lambda x: x / x.sum(), axis=0).fillna(0)
-    return df
+r = requests.get(REQUEST_URL).json()
+df = pd.read_json(r)
+#drop rows with empty topic column
+df = df[df['topic'].str.len() > 1]
+#drop rows with nan values in topic column
+df = df.dropna(subset=['topic'])
 
+#group df by topic and remove rows with only one date
+df = df.groupby('topic').filter(lambda x: len(x) > 1)
+#remove rows with less than 10 in count column
+df = df[df['count'] > 10]
+
+#group df by topic, group by the sum of values in "count" column for each topic, add the "representative_words" and "representative_articles" columns, and sort by the sum of values in descending order, Keep the topic column as a column instead of an index
+top_20 = df.groupby('topic').agg({'count': 'sum', 'representative_words': 'first', 'representative_articles': 'first'}).sort_values(by=['count'], ascending=False).reset_index()
+TOP_20_TOPICS = top_20.head(MAX_TOPICS)
+
+PROCESSED_DF = process_request(df)
+NUM_DOCUMENTS = df['count'].sum()
+# get the number of topics in PROCESSED_DF by grouping by the "topic" column and counting the number of groups
+NUM_TOPICS = len(df.groupby('topic'))
+FIRST_DAY = None
+LAST_DAY = None
+from pages.Sources import NUM_SOURCES
+
+
+st.markdown(f"**NewsWatch** is currently based on:")
+col1, col2, col3 = st.columns(3)
+col1.metric("RSS Feeds", f"{'{:,}'.format(NUM_SOURCES)}", help='Number of RSS feeds scraped')
+col2.metric("News Articles", f"{'{:,}'.format(NUM_DOCUMENTS)}", help='Number of news articles analyzed')
+col3.metric("Topics Extracted", f"{'{:,}'.format(NUM_TOPICS)}", help='Number of topics automatically extracted from the news articles')
+
+@st.cache
+def update_df(n_topics):
+    # Select first n topics
+    new_df = PROCESSED_DF.iloc[:n_topics, :]
+    # Convert document counts to percentages
+    new_df = new_df.apply(lambda x: x / x.sum(), axis=0).fillna(0)
+    new_df = new_df.drop(columns=[str(date.today())])
+    return new_df
+
+@st.cache
 def create_figure(df):
     x = df.columns.values.tolist()
     y = df.values.tolist()
     labels = df.index.values.tolist()
+    FIRST_DAY = x[0]
+    LAST_DAY = x[-1]
     fig = go.Figure()
-    for y_, label in zip(y, labels):
+    num_colors = len(y)
+    colors = [f'hsl({i * 360 // num_colors}, 100%, 50%)' for i in range(num_colors)]
+    shuffle(colors)
+    for y_, label, color in zip(y, labels, colors):
         fig.add_trace(
             go.Scatter(x=x,
                        y=y_,
                        name=label,
                        mode='lines',
-                       line=dict(width=0.5),
+                       line=dict(width=0.5, color=color),
                        line_shape='spline',
                        stackgroup='one',
                        groupnorm='percent',
@@ -102,33 +129,34 @@ def create_figure(df):
         showlegend=True,
         xaxis_type='category',
         yaxis=dict(type='linear', range=[1, 100], ticksuffix='%'),
-        title_text=f'Top {slider_val} topics between {x[0]} and {x[-1]}',
+        title_text=f'Last week in topics',
         title_x=0.3,
         title_font_size=20,
-        hovermode='x unified'
+        hovermode='x unified',
+        xaxis_title="Date",
+        yaxis_title="Articles per topic"
     )
     fig.update_xaxes(tickangle=45) #rotate x-axis labels
     return fig
 
-df = process_request()
+# Create a function to update the information panel based on the hovered legend label
+def update_info_panel(fig):
+    while True:
+        event = st.pydeck_chart(fig)
+        if event:
+            label = event["label"]
+            # Here, you can fetch and display additional information related to the hovered legend label
+            additional_info = f"Additional Info for {label}"  # Replace with your logic to fetch information
+            info_panel.text(additional_info)
 
-with st.form("submit_form"):
-    slider_val = st.slider('Select a number of topics', 1, MAX_TOPICS, 5)
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        n_topics = slider_val
-        df = update_df(df, n_topics)
-        #labels = df.index.get_level_values('Name').tolist()
-        fig = create_figure(df)
-        st.plotly_chart(fig, use_container_width=True, theme="streamlit", sharing="streamlit")
+update_info_panel(fig)  # Start listening for hover events
 
-# add a sidebar with an About section that explains the app
-
-st.sidebar.markdown('## About')
-
-st.sidebar.info('''
-NewsWatch is a prototype of a news cycle aggregator. It uses a topic modeling algorithm to extract topics from news articles and then visualizes the results in a streamgraph. The app is built with [Streamlit](https://streamlit.io/) and [Plotly](https://plotly.com/).
-''')
-
-# Place the logo at the bottom in the sidebar
-st.sidebar.image('3.png', width=300)
+if submitted:
+    n_topics = slider_val
+    df = update_df(n_topics)
+    fig = create_figure(df)
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit", sharing="streamlit")
+else:
+    df = update_df(slider_val)
+    fig = create_figure(df)
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit", sharing="streamlit")
