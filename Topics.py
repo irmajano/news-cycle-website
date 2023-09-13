17399577b57bfb11c9ba8d679500afcdac1e76cc
Ignacio Rubio Majano <rubiomajano.i@gmail.com>
2023-09-13 12:50:18 +0200

    Improved website's functionality

---
+++ Topics.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import requests
+import plotly.graph_objects as go
+import pandas as pd
+from pages.Sources import NUM_SOURCES
+from streamlit_card import card
--- pages/sources.py
+++ pages/sources.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title='Sources', layout='centered')
+st.set_page_config(page_title='ℹ️ Sources', layout='centered')
+st.title("Sources")
+# add a sidebar with an About section that explains the app
+st.sidebar.info('''
+NewsWatch is a prototype of a news cycle aggregator. It uses a topic modeling algorithm to extract topics from news articles and then visualizes the results in a streamgraph. The app is built with [Streamlit](https://streamlit.io/) and [Plotly](https://plotly.com/).
+''')
+# Place the logo at the bottom in the sidebar
+st.sidebar.image('newswatch_logo.png', use_column_width=True)
+
 
 # Define the API endpoint URL
 API_URL = 'https://news-cycle-aggregator-fudwhg6x5q-ew.a.run.app/get-sources'
